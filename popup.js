1b543b4b897afa9095365976c1be45b9569f8cf0
webwizarding <162193936+webwizarding@users.noreply.github.com>
2024-09-26 21:35:47 -0700

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   chrome.scripting.executeScript({
     target: { tabId: tabs[0].id },
     files: ["content.js", "fetch.js"],
+  }, () => {
+    console.log("Scripts executed successfully.");
+    // Any further actions you want to perform after script execution can be placed here.
+  });
 });
